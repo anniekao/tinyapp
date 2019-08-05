46522c451dc9e794de9ff0dfad9dba2caece2822
Annie <anniekao@posteo.net>
2019-08-05 13:34:09 -0400

    Added ejs to dependencies

--- express-server.js
+++ express-server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 7000; // defaul port is usually 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouse.ca",
   "9sm5xK": "http:www.google.com"
