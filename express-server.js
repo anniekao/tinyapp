852328ea3096a64e2944dad922b71279748040a2
Annie <anniekao@posteo.net>
2019-08-05 14:25:18 -0400

    Added header partial and urls_show.ejs

--- express-server.js
+++ express-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 7000; // defaul port is usually 8080
+const PORT = 8000; // defaul port is usually 8080
 
 app.set("view engine", "ejs");
 
@@ -19,12 +19,12 @@ app.get('/urls.json', (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("pages/urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.url};
-  res.render('urls_show', templateVars);
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('pages/urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
