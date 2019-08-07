450d09a8b0a59877193789b9444c857e4b16e047
Annie <anniekao@posteo.net>
2019-08-07 18:30:44 -0400

    Debugged redirect of shortURL

--- express-server.js
+++ express-server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const Keygrip = require('keygrip');
+const { getUserByEmail } = require('./helpers.js');
 
 const gripKeys = new Keygrip(["MUMBAI528", "LIGHT029"], "sha256", "hex");
 
@@ -23,15 +24,6 @@ const generateRandomString = () => {
   return result;
 };
 
-const findEmail = (email) => {
-  for (let user in users) {
-    if (users[user].email === email) {
-      return users[user];
-    }
-  }
-  return false;
-};
-
 const urlsForUser = (id) => {
   const result = {};
   for (let url in urlDatabase) {
@@ -42,7 +34,7 @@ const urlsForUser = (id) => {
   return result;
 };
 
-const users = {};
+const usersDatabase = {};
 const urlDatabase = {};
 
 app.get("/urls", (req, res) => {
@@ -50,7 +42,7 @@ app.get("/urls", (req, res) => {
   if (userId) {
     let templateVars = {
       urls: urlsForUser(userId),
-      user: users[userId]
+      user: usersDatabase[userId]
     };
     res.render("pages/urls_index", templateVars);
   } else {
@@ -62,7 +54,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   if (req.session.user_id) {
     let templateVars = {
-      user: users[req.session.user_id]
+      user: usersDatabase[req.session.user_id]
     };
     res.render("pages/urls_new", templateVars);
   } else {
@@ -72,7 +64,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/err", (req, res) => {
   let templateVars = {
-    user: users[req.session.user_id]
+    user: usersDatabase[req.session.user_id]
   };
   res.render("pages/urls_err", templateVars);
 });
@@ -85,7 +77,7 @@ app.get("/urls/:shortURL", (req, res) => {
       let templateVars = {
         shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL].longURL,
-        user: users[req.session.user_id]
+        user: usersDatabase[req.session.user_id]
       };
       res.render("pages/urls_show", templateVars);
     } else if (urlDatabase[req.params.shortURL] && Object.keys(urls).includes(req.params.shortURL) === false) {
@@ -98,22 +90,24 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
+
   if (!longURL) {
     res.redirect('urls/urls_err');
+  } else {
+    res.redirect(longURL);
   }
-  res.redirect(longURL);
 });
 
 app.get("/register", (req, res) => {
   let templateVars = {
-    user: users[req.session.user_id]
+    user: usersDatabase[req.session.user_id]
   };
   res.render('pages/urls_registration', templateVars);
 });
 
 app.get("/login", (req, res) => {
   let templateVars = {
-    user: users[req.session.user_id]
+    user: usersDatabase[req.session.user_id]
   };
 
   res.render('pages/login', templateVars);
@@ -150,11 +144,11 @@ app.post("/urls/:shortURL/update", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const user = findEmail(req.body.email);
+  const user = getUserByEmail(usersDatabase, req.body.email);
 
   if (req.body.email === "" || req.body.password === "") {
     res.status(400).send("Please fill out the email and/or password fields");
-  } else if (findEmail(req.body.email) === false) {
+  } else if (getUserByEmail(usersDatabase, req.body.email) === undefined) {
     res.status(400).send("Sorry you're not registered");
   } else if (bcrypt.compareSync(req.body.password, user.password) === false) {
     res.status(400).send("Wrong password!");
@@ -171,16 +165,16 @@ app.post("/logout", (req, res) => {
 
 app.post("/register", (req, res) => {
   let id = generateRandomString();
-  if (findEmail(req.body.email) !== false) {
+  if (getUserByEmail(usersDatabase, req.body.email)) {
     res.status(400).send("You've already registered");
   } else if (req.body.email === undefined || req.body.password === undefined) {
     res.status(400).send("Please fill out the password/email fields");
   }
 
-  users[id] = {};
-  users[id].user_id = id;
-  users[id].email = req.body.email;
-  users[id].password = bcrypt.hashSync(req.body.password, 10);
+  usersDatabase[id] = {};
+  usersDatabase[id].user_id = id;
+  usersDatabase[id].email = req.body.email;
+  usersDatabase[id].password = bcrypt.hashSync(req.body.password, 10);
 
   req.session.user_id = id;
   res.redirect('/urls');
