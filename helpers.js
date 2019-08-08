60a3c85473a042a42e0f58d6aef85611b7d73c44
Annie <anniekao@posteo.net>
2019-08-07 20:13:45 -0400

    Moved remaining helper functions to helper.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,9 @@
+// const addHTTPPrefix = (url) => {
+//   const httpwww = /\/\/www\./g;
+//   const www =  
+
+// }
+
 const getUserByEmail = (usersDb, email) => {
   for (let user in usersDb) {
     if (usersDb[user].email === email) {
@@ -6,4 +12,26 @@ const getUserByEmail = (usersDb, email) => {
   }
 };
 
-module.exports = { getUserByEmail };
+const generateRandomString = () => {
+  const ALPHA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(
+    ""
+  );
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += ALPHA[Math.floor(Math.random() * ALPHA.length)];
+  }
+  return result;
+};
+
+const urlsForUser = (urlsDatabase, id) => {
+  const result = {};
+  for (let url in urlsDatabase) {
+    if (urlsDatabase[url].userID === id) {
+      result[url] = urlsDatabase[url];
+    }
+  }
+  return result;
+};
+
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
